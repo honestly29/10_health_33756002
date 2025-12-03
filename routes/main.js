50a79462a0e5a4ad7ee8f7507cfd03e27035d300
Roan Jones <honestly2978@gmail.com>
2025-12-03 23:14:11 +0000

    Set up ejs

--- routes/main.js
+++ routes/main.js
@@ -3,12 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 // Handle the main routes
+router.get("/", (req, res) => {
+    res.render("index.ejs")
+}); 
 
-router.get("/", (req, res) => res.send("Hello World!")); 
-
-router.get("/about", (req, res) => res.send ("<h1>This is the about page</h1>"));
-
-
+router.get("/about", (req, res) => {
+    res.render("about.ejs")
+});
 
 // Export the router object so index.js can access it
 module.exports = router;
+
